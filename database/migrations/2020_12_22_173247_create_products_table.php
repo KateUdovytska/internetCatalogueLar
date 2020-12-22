06a806485f6908deb4c5d9fa26ef97dc56aa858a
kate <ketrina113@gmail.com>
2020-12-22 22:28:08 +0200

    migration database create producers and products

---
+++ database/migrations/2020_12_22_173247_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price');
+            $table->integer('producer_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('producer_id')
+                ->references('id')
+                ->on('producers');
+            $table->integer('user_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
