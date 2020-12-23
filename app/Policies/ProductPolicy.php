dc01fe41915bc70e6fa2caa05678ce325c1f6ae5
user <user@user.com>
2020-12-23 20:05:28 +0200

    Product policy

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+use App\User;
+use App\Model\Product;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    public function destroy(User $user, Product $product){
+        return $user->id === $product->user_id;
+    }
+
+}
