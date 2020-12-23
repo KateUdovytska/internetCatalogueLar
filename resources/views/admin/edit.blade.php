276bd61e804cf0f8a21bda2a5726a2cf2a41ab03
user <user@user.com>
2020-12-23 20:50:22 +0200

    edit product (a half)

---
+++ resources/views/admin/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.default')
+
+@section('content')
+
+    @include('common.errors')
+    <div class="panel-heading text-center">
+        <h2>@lang('messages.addPr')</h2>
+    </div>
+    <!-- Форма новой продукции -->
+    <form action="{{ route('admin.products.update') }}" method="POST" class="form-horizontal">
+    {{ csrf_field() }}
+    <!-- Имя задачи -->
+        <div class="form-group">
+            <label for="product" class="col-sm-3 control-label">@lang('messages.namePr')</label>
+            <div class="col-sm-6">
+                <input type="text" name="name" id="product" class="form-control" value="{{$product->name}}" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="description" class="col-sm-3 control-label">@lang('messages.description')</label>
+            <div class="col-sm-6">
+                <textarea class="form-control" name="description" id="description" required> value="{{$product->description}}"
+                </textarea>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="price" class="col-sm-3 control-label">@lang('messages.price')</label>
+            <div class="col-sm-6">
+                <input type="number" name="price" id="price" class="form-control" value="{{$product->price}}" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="producer" class="col-sm-3 control-label">@lang('messages.producer')</label>
+            <div class="col-sm-6">
+                <input type="text" name="producer" id="producer" class="form-control" value="{{$producer->name}}" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="producer_web" class="col-sm-3 control-label">@lang('messages.producer_web')</label>
+            <div class="col-sm-6">
+                <input type="text" name="web_site" id="producer_web" class="form-control" value="{{$product->web_site}}" required>
+            </div>
+        </div>
+
+        <!-- Кнопка добавления задачи -->
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-default">
+                    <i class="fa fa-plus"></i> @lang('messages.addPr')
+                </button>
+            </div>
+        </div>
+    </form>
+@endsection
